92a47896a5bef45ee1589e19ecfc6644532dd90d
chefcarroll <123791521+chefcarroll@users.noreply.github.com>
2023-12-10 13:47:52 -0800

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+onload = function getSize()
+{
+   let w = this.document.documentElement.clientWidth;
+   let h = this.document.documentElement.clientHeight;
+   this.document.getElementById('wh').innerHTML = "<h1> width: " + w + "<br  />" + "Height" + h + "<h1/>"
+
+   window.addEventListener('resize' , getSize);
+
+   
+}
+
+
